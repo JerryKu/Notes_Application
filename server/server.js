9ae07c59f51f35e293fede25fa92b5a1a3359596
Jerry Ku <jerryku1220@gmail.com>
2018-05-08 00:14:04 -0700

    added backend routes

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
+const db = require("./config.js");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const port = 8000;
+
+MongoClient.connect(db.uri, (err, database)=>{
+  if(err) return console.log(err)
+  let datab = database.db("notes_application")
+  require('./app/routes')(app, datab);
+
+  app.listen(port, ()=>{
+    console.log('server listening on port: ' + port);
+  });
+})
